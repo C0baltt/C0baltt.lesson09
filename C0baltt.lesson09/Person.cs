8ebbada08a55fe353972073aed98cfa3435a2284
Александр Жаворонок <C0baltt@live.com>
2020-09-22 16:19:21 +0300

    Добавьте файлы проекта.

---
+++ C0baltt.lesson09/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace C0baltt.lesson09
+{
+    public class Person
+    {
+        private string _name;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя не может быть пустым");
+            }
+            _name = name;
+        }
+    }
+}
---
+++ C0baltt.lesson09/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0baltt.lesson09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.SetName("");
+
+            Console.WriteLine(p.GetName());
+            Console.ReadLine();
+        }
+    }
+}
